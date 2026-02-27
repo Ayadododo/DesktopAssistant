2f6e885430c3738867144e7260f84c98fa0f9bbe
ChenHaobo <1072602853@qq.com>
2026-02-27 16:59:50 +0800

    项目内容基本搭建完毕

--- main.js
+++ main.js
@@ -1,37 +1,61 @@
 // main.js
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow,ipcMain } = require('electron')
 const path = require('path')
+
+
 
 // 创建一个函数来加载你的页面
 const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+    const mainWindow = new BrowserWindow({
+        width: 1024,
+        height:768,
+        resizable: false,
+        alwaysOnTop: true,
+        title:'防务桌面助手',
+        autoHideMenuBar: true,
+        frame: false,
         webPreferences: {
-            // 这里指定预加载脚本的位置，我们稍后会创建它
             preload: path.join(__dirname, 'preload.js'),
-            // 出于安全考虑，默认保持上下文隔离开启
             contextIsolation: true,
-            // 不要启用 nodeIntegration
             nodeIntegration: false
         }
     })
 
     // 加载 index.html 文件
-    win.loadFile('index.html')
+    mainWindow.loadURL('http://127.0.0.1:5173/');
 }
 
 // 当 Electron 完成初始化并准备好创建浏览器窗口时，调用此方法
 app.whenReady().then(() => {
     createWindow()
-
-    // macOS 应用通常即使在没有打开任何窗口的情况下也会继续运行
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
 })
 
 // 除了 macOS 外，当所有窗口都被关闭时，退出应用
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
+})
+
+
+let isSuspended = false          // 当前是否处于挂起状态
+const NORMAL_SIZE = { width: 1024, height: 768 }
+const SUSPEND_SIZE = { width: 600, height: 60 }
+
+// 处理挂起/恢复切换
+ipcMain.handle('toggle-suspend', async(event) => {
+    const mainWindow = BrowserWindow.fromWebContents(event.sender)
+    if (!mainWindow) return
+
+    if (isSuspended) {
+        // 恢复窗口
+        mainWindow.setResizable(true)
+        mainWindow.setSize(NORMAL_SIZE.width, NORMAL_SIZE.height)
+        mainWindow.setResizable(false) // 恢复
+        isSuspended = false
+    } else {
+        // 挂起窗口
+        mainWindow.setResizable(true)
+        mainWindow.setSize(SUSPEND_SIZE.width, SUSPEND_SIZE.height)
+        mainWindow.setResizable(false) // 恢复
+        isSuspended = true
+    }
 })
--- preload.js
+++ preload.js
@@ -1,12 +1,7 @@
 // preload.js
-// 这里暂时先写一个简单的示例，后续可以用来安全地暴露 API
-window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-        const element = document.getElementById(selector)
-        if (element) element.innerText = text
-    }
+const { contextBridge, ipcRenderer } = require('electron')
 
-    for (const type of ['chrome', 'node', 'electron']) {
-        replaceText(`${type}-version`, process.versions[type])
-    }
+contextBridge.exposeInMainWorld('electronAPI', {
+  // 切换挂起状态
+  toggleSuspend: () => ipcRenderer.invoke('toggle-suspend')
 })
